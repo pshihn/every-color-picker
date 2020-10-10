41ebb3b444d4b524ec0066ade9ee631da7e17511
Preet Shihn <preetshihn@gmail.com>
2020-10-10 01:20:13 -0700

    .

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import { terser } from "rollup-plugin-terser";
+import filesize from 'rollup-plugin-filesize';
+
+const INPUTS = [
+  'dino-color-picker',
+  'corel-color-picker',
+  'slider-color-picker',
+  'disk-color-picker',
+  'shop-color-picker',
+  'atom-color-picker',
+  'lucid-color-picker'
+]
+
+export default INPUTS.map((input) => {
+  return {
+    input: `./bin/${input}.js`,
+    output: {
+      file: `./packages/${input}/${input}.js`,
+      format: 'esm'
+    },
+    plugins: [
+      terser(),
+      filesize({
+        showMinifiedSize: false,
+        showBeforeSizes: 'build'
+      })
+    ]
+  }
+});
