3a1f48bb5c347a98245aeb49dc39258665e1791a
Preet Shihn <preetshihn@gmail.com>
2020-10-10 17:52:47 -0700

    .

--- create-packages.js
+++ create-packages.js
@@ -12,13 +12,16 @@ const INPUTS = [
   'lucid-color-picker'
 ];
 
+const VERSION = `0.0.1`;
+
 async function createPackages() {
   for (const name of INPUTS) {
     const template = `{
   "name": "${name}",
-  "version": "0.0.1",
+  "version": "${VERSION}",
   "description": "A color picker component",
   "main": "${name}.js",
+  "module": "${name}.js",
   "keywords": [
     "Color Picker",
     "ColorPicker",
